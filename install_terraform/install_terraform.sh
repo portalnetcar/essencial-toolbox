789d99c1b0b8e470167914c910e5a597325cd75a
Leonardo Savio <leosavio@leosavio.com>
2024-10-10 16:45:23 -0300

    Update install_terraform.sh

--- install_terraform/install_terraform.sh
+++ install_terraform/install_terraform.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Download Terraform
-curl -o ~/Downloads/terraform.zip https://releases.hashicorp.com/terraform/1.7.4/terraform_1.7.4_linux_amd64.zip
+curl -o ~/Downloads/terraform.zip https://releases.hashicorp.com/terraform/1.9.7/terraform_1.9.7_linux_amd64.zip
 
 # Unzip Terraform binary
 unzip ~/Downloads/terraform.zip -d ~/Downloads/
