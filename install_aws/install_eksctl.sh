e53aaf815da6d2a810f2597c8884f931d6a5f77e
Leonardo Savio <leosavio@leosavio.com>
2024-12-26 00:20:41 -0300

    Update install_eksctl.sh

--- install_aws/install_eksctl.sh
+++ install_aws/install_eksctl.sh
@@ -1,7 +1,14 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
 # for ARM systems, set ARCH to: `arm64`, `armv6` or `armv7`
 ARCH=amd64
 PLATFORM=$(uname -s)_$ARCH
 
+# Navigate to Downloads directory
+cd ~/Downloads
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 
 # (Optional) Verify checksum
